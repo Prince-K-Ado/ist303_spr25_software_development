cc636385d75f8a8c448ce101600556a5998de123
Leonardo <damapak@users.noreply.github.com>
2025-01-29 17:18:25 -0800

    week 2 addl

---
+++ scripts/if_fruit.py
@@ -0,0 +1,9 @@
+# if_fruit.py
+
+fruit = input("Enter a fruit: ")
+if fruit == "orange":
+  print(f"take a bite of {fruit}")
+elif fruit == "apple":
+  print(f"pass your {fruit} to a friend")
+else:
+  print(f"you forgot to bring your {fruit}")
